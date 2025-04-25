7f59bf918d8c96d2470cf8aeac1fb2249cafb00b
Hüseyin AKDERE <82976248+Pusatthsyn@users.noreply.github.com>
2025-04-25 16:33:48 +0300

    var & const variable types

---
+++ C05_OtherTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace C05_OtherTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 'var' ve 'const' degisken tipleri
+            var num = 5;           // Derleyici tipini int olarak belirler
+            var name = "Ali";      // Derleyici tipini string olarak belirler
+            var decimalNum = 3.55; // Derleyici tipini double olarak belirler
+            var key = false;       // Derleyici tipini bool olarak belirler
+            var character = 'C';   // Derleyici tipini char olarak belirler
+            Console.WriteLine(num + name + decimalNum + key + character);
+
+            const double PI = 3.14159; // Sabit degisken tanimlamasi, degeri degistirilemez
+            Console.WriteLine(PI);
+
+            // PI = 3114; // HATA: const ile tanimlanan degiskenler degistirilemez
+                       // const degiskenler tanimlandiklari anda deger alirlar ve sonradan degerleri degistirilemez
+        }
+    }
+}
