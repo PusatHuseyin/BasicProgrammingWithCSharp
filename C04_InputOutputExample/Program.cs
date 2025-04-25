7dae809861a6057918471d59651970cd181dadba
Hüseyin AKDERE <82976248+Pusatthsyn@users.noreply.github.com>
2025-04-25 16:04:58 +0300

    Input Output Example

---
+++ C04_InputOutputExample/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace C04_InputOutputExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Getting the name, age and height of the user
+
+            Console.Write("Your Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Your heiht: ");
+            float height = float.Parse(Console.ReadLine());
+            Console.WriteLine("Your name: " + name + " - Your Age: " + age + " - Your Height: " + height);
+
+            Console.Read();
+        }
+    }
+}
