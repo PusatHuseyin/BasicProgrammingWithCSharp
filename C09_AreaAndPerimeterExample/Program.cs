04c6f2086953389443022abbf4505997e82ac8f6
Hüseyin AKDERE <82976248+Pusatthsyn@users.noreply.github.com>
2025-04-27 13:33:29 +0300

    Perimeter and Area Calculation

---
+++ C09_AreaAndPerimeterExample/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace C09_AreaAndPerimeterExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Karenin ve Dikdörtgenin Alanları ve Çevrelerini Hesaplama
+            double squareEdge, rectangleShortSide, rectangleLongSide;
+            Console.Write("Enter square edge: ");
+            squareEdge = double.Parse(Console.ReadLine());
+            Console.WriteLine("Area of the square: " + (squareEdge * squareEdge));
+            Console.WriteLine("Perimeter of the square: " + (4 * squareEdge));
+
+            Console.Write("\nEnter the short side of the rectangle: ");
+            rectangleShortSide = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the long side of the rectangle: ");
+            rectangleLongSide = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Area of the rectangle: " + (rectangleShortSide * rectangleLongSide));
+            Console.WriteLine("Perimeter of the rectangle: " + (2 * (rectangleShortSide + rectangleLongSide)));
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ C10_CircleAreaPerimeter/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace C10_CircleAreaPerimeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dairenin cevresini ve alanini hesaplama - cevre --> 2πr, alan --> πr²
+
+            const double pi = 3.14159; // Pi sabitini tanimliyoruz
+            // double radius; // asagida 'var' olarak tanimlandi gerek kalmadi
+
+            Console.Write("Radius of the circle; "); // Kullanicidan yaricap isteniyor
+            var radius = Convert.ToDouble(Console.ReadLine()); // 'var' kullanilarak degiskenin veri tipi otomatik olarak belirlenir; burada girilen deger double oldugu icin radius degiskeni double olur
+
+            double perimeter = 2 * pi * radius;
+            double area = pi * radius * radius;
+
+            Console.WriteLine("Dairenin Cevresi: " + perimeter + "\nDairenin Alani: " + area);
+
+            Console.Read();
+        }
+    }
+}
