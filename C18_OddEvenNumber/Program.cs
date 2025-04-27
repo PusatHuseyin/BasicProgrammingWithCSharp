77745e478592c5f8c85be1d5a709feae6bf814e8
Hüseyin AKDERE <82976248+Pusatthsyn@users.noreply.github.com>
2025-04-27 22:05:34 +0300

    Odd or Even Number

---
+++ C18_OddEvenNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace C18_OddEvenNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Odd or Even Number
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 ==0)
+            {
+                Console.WriteLine("{0} is Even.", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is Odd.", num);
+            }
+            Console.Read();
+        }
+    }
+}
