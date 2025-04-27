6b18c570650f68ab1df41e958fc9373324f49580
Hüseyin AKDERE <82976248+Pusatthsyn@users.noreply.github.com>
2025-04-27 13:51:06 +0300

    Solving Mathematical Equations

---
+++ C11_MathEquationSolver/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace C11_MathEquationSolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // f(x) = ax² + bx + c 
+            // Delta (Δ) = b² - 4ac
+            // f(2), f(4) 
+
+            double a, b, c, x, result, delta;
+
+            // Kullanicidan a, b, c ve x degerlerini sirayla aliyoruz
+            Console.WriteLine("a, b, c ve x degerlerinin sirasiyla giriniz: ");
+            a = Convert.ToDouble(Console.ReadLine()); 
+            b = Convert.ToDouble(Console.ReadLine()); 
+            c = Convert.ToDouble(Console.ReadLine()); 
+            x = Convert.ToDouble(Console.ReadLine()); 
+
+            // Verilen x degeri icin f(x) fonksiyonunun sonucu hesaplaniyor
+            result = a * x * x + b * x + c; // ax² + bx + c 
+
+            // Delta degeri: b² - 4ac
+            delta = b * b - 4 * a * c;
+
+            Console.WriteLine("f({0}): " + result + "\nDelta: " + delta, x);
+
+            Console.Read();
+        }
+    }
+}
