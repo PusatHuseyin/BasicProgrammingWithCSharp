7e474531a61604e5c44ce474e658542730e568bc
Hüseyin AKDERE <82976248+Pusatthsyn@users.noreply.github.com>
2025-04-29 22:03:25 +0300

    Sum of numbers between 2 numbers

---
+++ C27_SumBetweenTwoNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace C27_SumBetweenTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2 sayi arasindaki sayilari toplama
+            Console.Write("Birinci sayiyi girin: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Ikinci sayiyi girin: ");
+            int num2 = int.Parse(Console.ReadLine());
+            int total = 0;
+            if (num1 > num2)
+            {
+                while (num1 >= num2)
+                {
+                    total += num1; 
+                    num1--; 
+                }
+                Console.WriteLine(num1 +"' den "+ num2+ "' ye kadar toplam: " + total);
+            }
+            else
+            {
+                while (num2 >= num1)
+                {
+                    total += num2;
+                    num2--;
+                }
+                Console.WriteLine(num2 + "' den " + num1 + "' ye kadar toplam: " + total);
+            }
+
+            Console.Read();
+        }
+    }
+}
