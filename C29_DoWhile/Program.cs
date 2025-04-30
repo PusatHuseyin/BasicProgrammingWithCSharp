9737bd1805ce415926b6cbb9514770ad7e6e0864
Hüseyin AKDERE <82976248+Pusatthsyn@users.noreply.github.com>
2025-04-30 20:24:12 +0300

    Do While

---
+++ C29_DoWhile/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace C29_DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // do while
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int orijinalNum = num;
+            int total = 0;
+            do
+            {
+                total += num;
+                num--;
+            }
+            while (num > 0);
+            Console.WriteLine($"The total is: {total}");
+
+            int total2 = 0;
+            while (orijinalNum > 0)
+            {
+                total2 += orijinalNum;
+                orijinalNum--;
+            }
+            Console.WriteLine($"The total is: {total2}");
+
+            Console.Read();
+        }
+    }
+}
