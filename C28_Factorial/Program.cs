cb876dd9ef0521d2edcf24c53d9160192d111f4e
Hüseyin AKDERE <82976248+Pusatthsyn@users.noreply.github.com>
2025-04-30 20:14:20 +0300

    Factorial

---
+++ C28_Factorial/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace C28_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Factorial
+            while (true)
+            {
+                Console.Write("Enter a number: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                int originalNumber = number;
+                int factorial = 1;
+                if (number >= 0)
+                {
+                    while (number > 0)
+                    {
+                        factorial *= number;
+                        number--;
+                    }
+                    Console.WriteLine($"{originalNumber}! = {factorial}");
+                }
+                else
+                {
+                    Console.WriteLine("Please do not enter numbers less than 0");
+                }
+            }
+          
+
+            
+        }
+    }
+}
