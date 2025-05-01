59019a49c872f3e79140f6204626140fd2255e1b
Hüseyin AKDERE <82976248+Pusatthsyn@users.noreply.github.com>
2025-05-01 21:15:13 +0300

    Foor Loop

---
+++ C33_ForLoop/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace C33_ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // with while
+            int num = 1;
+            while (num <= 10)
+            {
+                Console.WriteLine(num);
+                num++;
+            }
+
+            // with for
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write(i + " ");
+            }
+
+            int j = 1;
+            for (j = 0; j <= 10; j++)
+            {
+                Console.WriteLine(j);
+            }
+            Console.WriteLine("End j:" + j);
+            Console.Read();
+        }
+    }
+}
