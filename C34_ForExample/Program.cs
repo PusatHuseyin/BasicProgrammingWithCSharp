2ae57777bba201f193f86ab7e3e7225dabbf73e4
Hüseyin AKDERE <82976248+Pusatthsyn@users.noreply.github.com>
2025-05-01 21:31:53 +0300

    For Example 1

---
+++ C34_ForExample/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace C34_ForExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 0'den kullnicidan alinan sayiya kadar istedigi araliklarla yazdirma
+            Console.Write("Sayi girin: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Artis miktarini girin: ");
+            int artis = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= num; i += artis)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.Read();
+        }
+    }
+}
